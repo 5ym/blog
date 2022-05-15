ef7f6fefbc2e5ca32cf3bc01020fbca01d47bfce
5ym <16yuim@gmail.com>
2022-05-15 13:46:34 +0900

    fix image

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,12 @@
 import NextImage, { ImageProps } from 'next/image'
+import Link from 'next/link'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => (
+  <Link href={rest.src as string}>
+    <a target="_blank">
+      <NextImage {...rest} />
+    </a>
+  </Link>
+)
 
 export default Image
