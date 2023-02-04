23333377a2917bc64cf7c212b160edd917567edb
Ruk Doe <info@daco.dev>
2023-02-04 16:00:57 +0900

    fix security policy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com pagead2.googlesyndication.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' data: giscus.app www.googletagmanager.com pagead2.googlesyndication.com;
   style-src 'self' 'unsafe-inline' www.googletagmanager.com fonts.googleapis.com;
   img-src * blob: data:;
   media-src *;
