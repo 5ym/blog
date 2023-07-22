e8bc388b50d8e95c6398927de1bcf0d65240bc94
Ruk Doe <info@doany.io>
2023-07-22 21:39:53 +0900

    fix adsense

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,4 +1,5 @@
-import { useEffect, useRouter } from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const AdBanner = (props) => {
   const { asPath } = useRouter()
