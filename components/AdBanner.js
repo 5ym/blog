754185660ff66a562b1f3b521553934cd62887a6
Ruk Doe <info@doany.io>
2023-07-22 21:26:02 +0900

    fix adsense

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,14 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useRouter } from 'react'
 
 const AdBanner = (props) => {
+  const { asPath } = useRouter()
   useEffect(() => {
     try {
       ;(window.adsbygoogle = window.adsbygoogle || []).push({})
     } catch (err) {
       console.log(err)
     }
-  }, [])
+  }, [asPath])
 
-  return <ins className="adsbygoogle mt-4" data-ad-client="ca-pub-4568277783171675" {...props} />
+  return (
+    <ins
+      key={asPath}
+      className="adsbygoogle mt-4"
+      data-ad-client="ca-pub-4568277783171675"
+      {...props}
+    />
+  )
 }
 export default AdBanner
