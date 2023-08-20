0cb9ff6e67eda6dba446fa151b4dd3d685e4d435
Ruk Doe <info@doany.io>
2023-08-20 12:00:51 +0900

    fix adsense

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,16 +1,13 @@
-import Script from "next/script"
+import Script from 'next/script'
 
-const AdBanner = (props) => {
-  return (<>
-    <ins
-      className="adsbygoogle mt-4"
-      data-ad-client="ca-pub-4568277783171675"
-      {...props}
-    />
-    <Script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-      console.log(window.adsbygoogle)
-    </Script>
-  </>)
+const AdBanner = (props: { 'data-ad-slot': string }) => {
+  return (
+    <>
+      <ins className="adsbygoogle mt-4" data-ad-client="ca-pub-4568277783171675" {...props} />
+      <Script id={props['data-ad-slot']}>
+        (adsbygoogle = window.adsbygoogle || []).push({}); console.log(window.adsbygoogle)
+      </Script>
+    </>
+  )
 }
 export default AdBanner
