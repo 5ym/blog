ad877d1730d0e07f996c6aca81d73b76dfdb458e
Ruk Doe <info@doany.io>
2023-09-05 20:35:08 +0900

    fix adsense

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -11,7 +11,6 @@ const AdBanner = (props: {
   return (
     <div key={pathName} className={props.className}>
       <ins
-        key={pathName}
         data-ad-client="ca-pub-4568277783171675"
         data-ad-slot={props['data-ad-slot']}
         data-ad-format={props['data-ad-format']}
