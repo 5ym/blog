7a6a08258df1deda33aa99f12e66414e9d81ad1b
Ruk Doe <info@doany.io>
2023-09-05 21:20:24 +0900

    fix adsense

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -9,13 +9,14 @@ const AdBanner = (props: {
 }) => {
   const pathName = usePathname()
   return (
-    <div key={pathName} className={props.className}>
+    <div key={pathName} className={'w-full ' + props.className}>
       <ins
         data-ad-client="ca-pub-4568277783171675"
         data-ad-slot={props['data-ad-slot']}
         data-ad-format={props['data-ad-format']}
         data-full-width-responsive={props['data-full-width-responsive']}
-        className="adsbygoogle w-full"
+        className="adsbygoogle"
+        style={{ display: 'block' }}
       />
       <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </div>
