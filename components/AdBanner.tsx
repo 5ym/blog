d24465f7b2798e9685ccab75c9f0dc8bac66dafe
Ruk Doe <info@doany.io>
2023-09-05 17:57:59 +0900

    fix adsense

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -4,6 +4,13 @@ const AdBanner = (props: {
   'data-full-width-responsive': string
   className: string
 }) => {
-  return <ins style={{ display: 'block' }} data-ad-client="ca-pub-4568277783171675" {...props} />
+  return (
+    <ins
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-4568277783171675"
+      {...props}
+      className={'adsbygoogle ' + props.className}
+    />
+  )
 }
 export default AdBanner
