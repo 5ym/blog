98cf30ebfd9b8acbd362f05ade91adb840ccd20a
Ruk Doe <info@doany.io>
2023-09-06 14:25:27 +0900

    fix

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { usePathname } from 'next/navigation'
-import { useCallback, useEffect } from 'react'
+import Script from 'next/script'
 
 const AdBanner = (props: {
   'data-ad-slot': string
@@ -8,30 +8,33 @@ const AdBanner = (props: {
   'data-full-width-responsive': string
   className: string
 }) => {
-  const LoadAds = useCallback(() => {
-    if (window.adsbygoogle === undefined) {
-      const script = document.createElement('script')
-      script.src =
-        'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4568277783171675'
-      script.async = true
-      document.body.appendChild(script)
-    } else {
-      console.log('loaded')
-      window.adsbygoogle?.push({})
-    }
-  }, [])
-  useEffect(() => {
-    LoadAds()
-  }, [LoadAds])
+  const pathName = usePathname()
   return (
-    <ins
-      data-ad-client="ca-pub-4568277783171675"
-      data-ad-slot={props['data-ad-slot']}
-      data-ad-format={props['data-ad-format']}
-      data-full-width-responsive={props['data-full-width-responsive']}
-      className={'adsbygoogle w-full ' + props.className}
-      style={{ display: 'block' }}
-    />
+    <div key={pathName} className={'w-full ' + props.className}>
+      <ins
+        data-ad-client="ca-pub-4568277783171675"
+        data-ad-slot={props['data-ad-slot']}
+        data-ad-format={props['data-ad-format']}
+        data-full-width-responsive={props['data-full-width-responsive']}
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+      />
+      <Script
+        id="ads"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4568277783171675"
+        async
+        onReady={() => {
+          let noError = true
+          while (noError) {
+            try {
+              window.adsbygoogle?.push({})
+            } catch (error) {
+              noError = false
+            }
+          }
+        }}
+      />
+    </div>
   )
 }
 export default AdBanner
